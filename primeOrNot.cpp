463131a37b1246a26fb8f6eb32ae9b15c732913a
Neeraja-Rajan <65912613+Neeraja-Rajan@users.noreply.github.com>
2021-10-01 00:57:00 +0530

    Update primeOrNot.cpp

--- primeOrNot.cpp
+++ primeOrNot.cpp
@@ -14,7 +14,10 @@ int main()
     {
         for(int i=2;i<num;i++)
         {
-            
+            if(num%i==0)
+        {
+            div++;              
+        }
         }
         if(flag==1)
         {
