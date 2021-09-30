c3caeb1a9334b6a84cd3895ea077672f782ce6f8
Philona Reetha Sebastian <60103472+philona123@users.noreply.github.com>
2021-09-30 18:59:12 +0530

    added code

---
+++ primeOrNot.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    int flag=1;
+    cout<<"\nEnter a number: ";
+    cin>>num;
+    if(num==0||num==1)
+    {
+        cout<<"\nNeither prime nor composite.";
+    }
+    else
+    {
+        for(int i=2;i<num;i++)
+        {
+            
+        }
+        if(flag==1)
+        {
+        cout<<"\nPrime";
+    }
+    else
+    {
+        cout<<"\nNot Prime";
+    }
+    }
+    
+    return 0;
+}
