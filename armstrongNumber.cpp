d762d825411c22ebb3ae1a4ccedae932812417d8
Philona Reetha Sebastian <60103472+philona123@users.noreply.github.com>
2021-09-30 19:06:23 +0530

    added code

---
+++ armstrongNumber.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+    #include<math.h>
+    using namespace std;
+    //main Program
+    int main()
+    {
+        int num, digit, sum = 0;
+        cout << “Enter a positive  integer: “;
+        //user input
+        cin >> num;
+        int store = num;
+        //find sum of cubes of individual digits
+        do
+        {
+            
+        }while(num != 0);
+        //checking for ArmStrong number
+        if(sum == store)    
+            cout << store << ” is an Armstrong number.”;
+        else
+            cout << store << ” is not an Armstrong number.”;
+        return 0;
+    }
