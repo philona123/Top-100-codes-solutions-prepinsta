648feaaf1127218b8590021915d9d4ef85e1ae91
Philona Reetha Sebastian <60103472+philona123@users.noreply.github.com>
2021-09-30 19:04:45 +0530

    added code

---
+++ palindrome.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+    using namespace std;
+    //main Program
+    int main()
+    {
+        int num, digit, reverse = 0;
+        cout << “Enter a positive integer: “;
+        //user input
+        cin >> num;
+        int temp = num;
+        //loop to find reverse
+        do
+        {
+            
+        } while (num != 0);
+        cout << “The reverse of “<< temp <<” is “<< reverse << endl;
+        //checking for palindrome
+        if (temp == reverse)
+            cout << “The number is a palindrome.”;
+        else
+            cout << “The number is not a palindrome.”;
+        return 0;
+    }
